efdb91af462f82e87595f84ada0b2f925197e034
Andrew <andrew8gmf@gmail.com>
2021-03-18 12:06:30 -0300

    Remove: Useless standard files Add: Basic routes structure

---
+++ web/src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Login</h1>} />
+      <Route path="/signup" component={() => <h1>SignUp</h1>} />
+      <Route path="/app" component={() => <h1>App</h1>} />
+      <Route path="*" component={() => <h1>Page not found</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
