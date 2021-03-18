c5073b2d45cf2cb39620333782c66023d5871bc1
andrew8gmf <andrew8gmf@gmail.com>
2021-03-18 13:55:29 -0300

    Add: Private route

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import SignUp from "./pages/SignUp";
+
+import { isAuthenticated } from "./services/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <h1>Login</h1>} />
       <Route path="/signup" component={SignUp} />
-      <Route path="/app" component={() => <h1>App</h1>} />
+      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
