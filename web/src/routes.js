b30b54c58c8e9dac379653ab00f24573223fab28
Andrew <andrew8gmf@gmail.com>
2021-03-18 12:47:36 -0300

    Add: GlobalStyles and styled SignUp page

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import SignUp from "./pages/SignUp";
+
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <h1>Login</h1>} />
-      <Route path="/signup" component={() => <h1>SignUp</h1>} />
+      <Route path="/signup" component={SignUp} />
       <Route path="/app" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
