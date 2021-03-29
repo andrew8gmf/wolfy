3b99c5343d45508bb21ed4a7d3d7264134d466fb
Andrew <andrew8gmf@gmail.com>
2021-03-29 11:42:26 -0300

    Update: Get user information by login

--- web/src/services/auth.js
+++ web/src/services/auth.js
@@ -1,9 +1,13 @@
 export const TOKEN_KEY = "@wolfey-token";
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const USER_KEY = "@wolfey-user";
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) && localStorage.getItem(USER_KEY)  !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const login = token => {
+export const getUser = () => localStorage.getItem(USER_KEY);
+export const login = (token, user) => {
   localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(USER_KEY, user);
 };
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(USER_KEY);
 };
